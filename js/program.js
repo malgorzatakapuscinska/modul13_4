eeb5241c2cc0ab134ef1adce10e7fbb6bf1edb91
Malgorzata Kapuscinska <m_kapuscinska1@o2.pl>
2018-02-14 22:55:31 +0100

    Add sysOSinfo to program.js

--- js/program.js
+++ js/program.js
@@ -15,8 +15,30 @@ process.stdin.on('readable', function() {
                 process.stdout.write('Quitting app!\n');
                 process.exit();
                 break;
+            case '/sysOSinfo':
+            	getOSinfo();
+            	break;
             default:
                 process.stderr.write('Wrong instruction!\n');
         };
     }
 });
+
+function getOSinfo() {
+    var type = os.type();
+    if(type === 'Darwin') {
+        type = 'OSX';
+    } else if(type === 'Windows_NT') {
+        type = 'Windows';
+    }
+    var release = os.release();
+    var cpu = os.cpus()[0].model;
+    var uptime = os.uptime();
+    var userInfo = os.userInfo();
+    console.log('System:', type);
+    console.log('Release:', release);
+    console.log('CPU model:', cpu);
+    console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
+    console.log('User name:', userInfo.username);
+    console.log('Home dir:', userInfo.homedir);
+}
